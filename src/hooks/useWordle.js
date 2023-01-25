d46dff1df3ba10fc37f894f14b2d49bac0334d4a
Neoh Wei Jun <neohwj28@gmail.com>
2023-01-25 10:12:06 +0800

    Check condition for guesses submission

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -4,27 +4,50 @@ const useWordle = (solution) => {
 	const [turn, setTurn] = useState(0); // Track turns (6 guesses = game over)
 	const [currentGuess, setCurrentGuess] = useState(""); // Track what user is typing
 	const [guesses, setGuesses] = useState([]); // Each guess is an array
-	const [history, setHistory] = useState([]); // Each guess is a string
+	const [history, setHistory] = useState(["hello", "nihao"]); // Each guess is a string
 	const [isCorrect, setIsCorrect] = useState(false); // When user win = True
 
 	// Format a guess into an array of letter objects
 	// e.g., [{key: "a", color: "yellow"}]
 
-	const formatGuess = () => {};
+	const formatGuess = () => {
+		console.log("Formatting the guess", currentGuess);
+	};
 
 	// Add a new guess to the guesses state
 	// Update the isCorrect state if the guess is correct
 	// Add one to the turn state
 	const addNewGuess = () => {};
 
+	// Fires everytime the user enters any key on the keyboard
 	// Handle keyup event & track current guess
 	// If user presses enter, add the new guess
 	const handleKeyUp = ({ key }) => {
-		// Use regex to check for alphabets only
-		// Test the key against regex pattern to compare matches
+		if (key === "Enter") {
+			// Only add guess if turn is less than 5
+			if (turn > 5) {
+				console.log("You used all the guesses");
+				return;
+			}
 
-		// If backspace if pressed, delete the last letter
+			// Do not allow duplicated word / submission
+			if (history.includes(currentGuess)) {
+				console.log("you already tried that word.");
+				return;
+			}
+
+			// Word must be 5 characters long
+			if (currentGuess.length !== 5) {
+				console.log("Word must be 5 chars long");
+				return;
+			}
+
+			// Insert the guess as valid input
+			formatGuess(currentGuess);
+		}
+
 		if (key === "Backspace") {
+			// If backspace if pressed, delete the last letter
 			setCurrentGuess((prev) => {
 				// Return a new string based on the old stirng, removes the last character
 				return prev.slice(0, -1);
@@ -33,6 +56,8 @@ const useWordle = (solution) => {
 			return;
 		}
 
+		// Use regex to check for alphabets only
+		// Test the key against regex pattern to compare matches
 		if (/^[A-Za-z]$/.test(key)) {
 			if (currentGuess.length < 5) {
 				setCurrentGuess((prev) => {
