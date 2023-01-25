f10e796d638dec856dbb38371e82424f8a6e5878
Neoh Wei Jun <neohwj28@gmail.com>
2023-01-25 13:40:21 +0800

    Show the current guess on the brackets

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -5,6 +5,10 @@ export default function Grid({ currentGuess, guesses, turn }) {
 	return (
 		<div>
 			{guesses.map((guess, index) => {
+				if (turn === index) {
+					return <Row key={index} currentGuess={currentGuess} />;
+				}
+
 				// 6 Rows
 				return <Row key={index} guess={guess} />;
 			})}
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Row({ guess }) {
+export default function Row({ guess, currentGuess }) {
 	// If not undefined
 	if (guess) {
 		return (
@@ -9,6 +9,27 @@ export default function Row({ guess }) {
 					<div key={index} className={letter.color}>
 						{letter.key}
 					</div>
+				))}
+			</div>
+		);
+	}
+
+	// Check for the current guess props
+	if (currentGuess) {
+		let letters = currentGuess.split("");
+
+		return (
+			<div className="row current">
+				{letters.map((letter, index) => (
+					<div key={index} className="filled">
+						{letter}
+					</div>
+				))}
+
+				{/* Create an empty array with undefined value for the five brackets */}
+				{[...Array(5 - letters.length)].map((value, index) => (
+					// Empty squares in the row
+					<div key={index}></div>
 				))}
 			</div>
 		);
